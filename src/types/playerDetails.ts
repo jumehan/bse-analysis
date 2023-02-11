fd3a55f610e7ed7572ee2cc3707b04245736c2eb
Julie Han <juliemenghan@gmail.com>
2023-02-11 17:18:01 -0500

    new type player details

---
+++ src/types/playerDetails.ts
@@ -0,0 +1,48 @@
+export interface PlayerDetails {
+  get?: string;
+  parameters?: {
+    id?: string;
+    [k: string]: unknown;
+  };
+  errors?: unknown[];
+  results?: number;
+  response?: {
+    id?: number;
+    firstname?: string;
+    lastname?: string;
+    birth?: {
+      date?: string;
+      country?: string;
+      [k: string]: unknown;
+    };
+    nba?: {
+      start?: number;
+      pro?: number;
+      [k: string]: unknown;
+    };
+    height?: {
+      feets?: null;
+      inches?: null;
+      meters?: null;
+      [k: string]: unknown;
+    };
+    weight?: {
+      pounds?: null;
+      kilograms?: null;
+      [k: string]: unknown;
+    };
+    college?: null;
+    affiliation?: string;
+    leagues?: {
+      standard?: {
+        jersey?: number;
+        active?: boolean;
+        pos?: null;
+        [k: string]: unknown;
+      };
+      [k: string]: unknown;
+    };
+    [k: string]: unknown;
+  }[];
+  [k: string]: unknown;
+}
