89743729d7e8bcdfe0d63278ec7953171c7f8174
Julie Han <juliemenghan@gmail.com>
2023-02-13 22:34:02 -0500

    ui changes

--- src/pages/PlayerProfile.tsx
+++ src/pages/PlayerProfile.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Col, Row } from "reactstrap";
 import nbaApi from "../api/nbaApi";
 import nbaData from "../api/nbaData";
 import PlayerProfileCard from "../components/ui-components/PlayerProfileCard";
 import PlayerProfileList from "../components/ui-components/PlayerProfileList";
+import PlayerStatsTable from "../components/ui-components/PlayerStatsTable";
+import PlayerTeamCard from "../components/ui-components/PlayerTeamCard";
 import { PlayerDetailsData } from "../types/playerDetails";
+import { PlayerStatsData } from "../types/playerStats";
 
 interface PlayerProfile {
-  data?: PlayerDetailsData;
+  details?: PlayerDetailsData;
+  stats?: PlayerStatsData[];
   personId?: string;
   isLoading: boolean;
 }
@@ -15,22 +20,23 @@ interface PlayerProfile {
 /**  */
 function PlayerProfile() {
   const [player, setPlayer] = useState<PlayerProfile>({
-    data: undefined,
+    details: undefined,
+    stats: undefined,
     personId: undefined,
     isLoading: true,
   });
   const { id } = useParams();
+  const season = "2022";
 
   useEffect(
     function fetchPlayerDetail() {
       async function fetchPlayer() {
-        const result = await nbaApi.getPlayerDetails(id || "2855");
-        const personId = nbaData.getPlayerId(
-          result?.response[0].firstname,
-          result?.response[0].lastname
-        );
+        const playerDetails = await nbaApi.getPlayerDetails(id || "2855");
+        const playerStats = await nbaApi.getPlayerStats(id || "2855", season);
+        const personId = nbaData.getPlayerId(playerDetails?.response[0].firstname, playerDetails?.response[0].lastname);
         setPlayer({
-          data: result?.response[0],
+          details: playerDetails.response[0],
+          stats: playerStats.response,
           personId: personId,
           isLoading: false,
         });
@@ -40,15 +46,20 @@ function PlayerProfile() {
     [id]
   );
 
-  if (player.isLoading || !player.data || !player.personId)
-    return <i>Loading...</i>;
+  if (player.isLoading || !player.details || !player.personId || !player.stats) return <i>Loading...</i>;
 
   return (
     <React.Fragment>
-      <div className="row align-items-start">
-        <PlayerProfileCard player={player.data} id={player.personId} />
-        <PlayerProfileList player={player.data} />
-      </div>
+      <Row className="align-items-start">
+        <Col sm="4" className="px-2">
+          <PlayerProfileCard player={player.details} id={player.personId} />
+        </Col>
+        <Col className="px-2" sm="8">
+          <PlayerTeamCard team={player.stats[0].team} player={player.details} />
+          <PlayerProfileList player={player.details} />
+        </Col>
+        <PlayerStatsTable stats={player.stats} season={season} />
+      </Row>
     </React.Fragment>
   );
 }
