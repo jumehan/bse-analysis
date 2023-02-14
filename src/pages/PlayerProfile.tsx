a3e51b2d6241c5d5521916f549e0298ee55675b9
Julie Han <juliemenghan@gmail.com>
2023-02-14 17:02:17 -0500

    layout edits

--- src/pages/PlayerProfile.tsx
+++ src/pages/PlayerProfile.tsx
@@ -5,10 +5,12 @@ import nbaApi from "../api/nbaApi";
 import nbaData from "../api/nbaData";
 import PlayerProfileCard from "../components/ui-components/PlayerProfileCard";
 import PlayerProfileList from "../components/ui-components/PlayerProfileList";
-import PlayerStatsTable from "../components/ui-components/PlayerStatsTable";
+import PlayerShootingStatsTable from "../components/ui-components/PlayerShootingStatsTable";
 import PlayerTeamCard from "../components/ui-components/PlayerTeamCard";
 import { PlayerDetailsData } from "../types/playerDetails";
 import { PlayerStatsData } from "../types/playerStats";
+import { Row as R } from "react-table";
+import GlossaryList from "../components/ui-components/GlossaryList";
 
 interface PlayerProfile {
   details?: PlayerDetailsData;
@@ -33,7 +35,10 @@ function PlayerProfile() {
       async function fetchPlayer() {
         const playerDetails = await nbaApi.getPlayerDetails(id || "2855");
         const playerStats = await nbaApi.getPlayerStats(id || "2855", season);
-        const personId = nbaData.getPlayerId(playerDetails?.response[0].firstname, playerDetails?.response[0].lastname);
+        const personId = nbaData.getPlayerId(
+          playerDetails?.response[0].firstname,
+          playerDetails?.response[0].lastname
+        );
         setPlayer({
           details: playerDetails.response[0],
           stats: playerStats.response,
@@ -46,7 +51,15 @@ function PlayerProfile() {
     [id]
   );
 
-  if (player.isLoading || !player.details || !player.personId || !player.stats) return <i>Loading...</i>;
+  const renderRowSubComponent = (row: R<PlayerStatsData>) => {
+    const {
+      game: { id },
+    } = row.original;
+    return <h5>{id}</h5>;
+  };
+
+  if (player.isLoading || !player.details || !player.personId || !player.stats)
+    return <i>Loading...</i>;
 
   return (
     <React.Fragment>
@@ -58,8 +71,14 @@ function PlayerProfile() {
           <PlayerTeamCard team={player.stats[0].team} player={player.details} />
           <PlayerProfileList player={player.details} />
         </Col>
-        <PlayerStatsTable stats={player.stats} season={season} />
+        <PlayerShootingStatsTable
+          data={player.stats}
+          season={season}
+          renderRowSubComponent={renderRowSubComponent}
+        />
       </Row>
+      <hr />
+      <GlossaryList />
     </React.Fragment>
   );
 }
