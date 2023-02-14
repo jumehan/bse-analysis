acdd51c7d4cad4bd5ce4d485344281c10b03b518
Julie Han <juliemenghan@gmail.com>
2023-02-14 17:01:01 -0500

    docstrings

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Container } from "reactstrap";
 import NavBar from "./components/layout/NavBar";
 import RouteList from "./components/routes/RouteList";
 
-function App() {
+/** React App Component
+ * { App } -> { NavBar } { RouteList }
+ * @returns {JSX.Element} App Component
+ */
+function App(): JSX.Element {
   return (
     <Container className="my-4">
       <BrowserRouter>
