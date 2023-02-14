2bf982840f344920ed6a5812c640881ed2581b7f
Julie Han <juliemenghan@gmail.com>
2023-02-13 22:33:53 -0500

    sep interface into segments

--- src/types/playerStats.ts
+++ src/types/playerStats.ts
@@ -7,48 +7,50 @@ export interface PlayerStats {
   };
   errors?: unknown[];
   results?: number;
-  response?: {
-    player?: {
-      id?: number;
-      firstname?: string;
-      lastname?: string;
-      [k: string]: unknown;
-    };
-    team?: {
-      id?: number;
-      name?: string;
-      nickname?: string;
-      code?: string;
-      logo?: string;
-      [k: string]: unknown;
-    };
-    game?: {
-      id?: number;
-      [k: string]: unknown;
-    };
-    points?: number;
-    pos?: string;
-    min?: string;
-    fgm?: number;
-    fga?: number;
-    fgp?: string;
-    ftm?: number;
-    fta?: number;
-    ftp?: string;
-    tpm?: number;
-    tpa?: number;
-    tpp?: string;
-    offReb?: number;
-    defReb?: number;
-    totReb?: number;
-    assists?: number;
-    pFouls?: number;
-    steals?: number;
-    turnovers?: number;
-    blocks?: number;
-    plusMinus?: string;
-    comment?: null;
+  response: PlayerStatsData[];
+  [k: string]: unknown;
+}
+
+export interface PlayerStatsData {
+  player?: {
+    id?: number;
+    firstname?: string;
+    lastname?: string;
     [k: string]: unknown;
-  }[];
+  };
+  team: {
+    id?: number;
+    name: string;
+    nickname?: string;
+    code?: string;
+    logo?: string;
+    [k: string]: unknown;
+  };
+  game: {
+    id?: number;
+    [k: string]: unknown;
+  };
+  points?: number;
+  pos?: string;
+  min?: string;
+  fgm?: number;
+  fga?: number;
+  fgp?: string;
+  ftm?: number;
+  fta?: number;
+  ftp?: string;
+  tpm?: number;
+  tpa?: number;
+  tpp?: string;
+  offReb?: number;
+  defReb?: number;
+  totReb?: number;
+  assists?: number;
+  pFouls?: number;
+  steals?: number;
+  turnovers?: number;
+  blocks?: number;
+  plusMinus?: string;
+  comment?: null;
   [k: string]: unknown;
 }
