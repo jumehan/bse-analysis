c747833b513b2c4ad84196352732fe2dd3321a9a
Julie Han <juliemenghan@gmail.com>
2023-02-14 23:20:31 -0500

    init search form component commit

---
+++ src/components/ui-components/SearchForm.tsx
@@ -0,0 +1,68 @@
+import { debounce } from "lodash";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { FormGroup, Input, Label, ListGroupItem } from "reactstrap";
+import nbaApi from "../../api/nbaApi";
+
+interface SearchResults {
+  id: number;
+  firstname: string;
+  lastname: string;
+}
+
+function SearchForm() {
+  const [playerList, setPlayerList] = useState<Array<SearchResults>>([]);
+
+  async function search(name: string) {
+    const resp = await nbaApi.getPlayers(name);
+    return resp.response;
+  }
+
+  const debouncedSearch = useRef(
+    debounce(async (name) => {
+      const results = await search(name);
+      setPlayerList(
+        results.map((player) => ({
+          id: player.id,
+          firstname: player.firstname,
+          lastname: player.lastname,
+        }))
+      );
+    }, 300)
+  ).current;
+
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [debouncedSearch]);
+
+  /** Event Handler
+   * Ensure that the value is not empty before passing it to the search function */
+  async function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.value.length > 0) debouncedSearch(e.target.value);
+  }
+
+  return (
+    <FormGroup>
+      <Label for="search"></Label>
+      <Input
+        id="seach"
+        name="search"
+        className="small"
+        bsSize="sm"
+        placeholder="Search for player by last name, ex: James for Lebron James"
+        type="search"
+        onChange={handleChange}
+      />
+      <ul className="glossary py-2">
+        {playerList.map((player) => (
+          <ListGroupItem key={player.id} action href={`${player.id}`} tag="a">
+            {player.firstname} {player.lastname}
+          </ListGroupItem>
+        ))}
+      </ul>
+    </FormGroup>
+  );
+}
+
+export default SearchForm;
