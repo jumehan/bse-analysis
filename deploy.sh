b7aaa5e93c0f3b6cb26542fdcf45a99507fd967c
Julie Han <juliemenghan@gmail.com>
2023-02-15 00:10:26 -0500

    debugging deployment

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 echo > .nojekyll
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'https://jumehan.github.io/' > CNAME
 
 git init
 git checkout -B main
