cb83ebfba1d84a412d7e1984e052c7cbcd87a34a
Julie Han <juliemenghan@gmail.com>
2023-02-14 23:16:55 -0500

    formatting

--- src/api/nbaData.ts
+++ src/api/nbaData.ts
@@ -1,8 +1,7 @@
-import * as players from "../data/nbaPlayers.json" assert { type: 'JSON' };
+import * as players from "../data/nbaPlayers.json" assert { type: "JSON" };
 
 /** Class for managing data from json file obtained from NBA Data API */
 class nbaData {
-
   /**
    * @param {string} firstname
    * @param {string} lastname
@@ -10,11 +9,10 @@ class nbaData {
    */
   static getPlayerId(firstname: string, lastname: string): string {
     const player = players.league.standard.filter(
-      (player) =>
-        player.firstName === firstname && player.lastName === lastname
+      (player) => player.firstName === firstname && player.lastName === lastname
     );
     console.debug("getPlayerId", player);
-    return player[0].personId;
+    return player.length ? player[0].personId : "";
   }
 }
 
