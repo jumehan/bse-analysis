fef55486e6cfd765233c6e0dab1cdca37646bd06
Julie Han <juliemenghan@gmail.com>
2023-02-16 00:00:16 -0500

    added team type

---
+++ src/types/teams.ts
@@ -0,0 +1,24 @@
+export interface Team {
+  id: number;
+  name: string;
+  nickname: string;
+  code: string;
+  city: string;
+  logo: string;
+  allStar: boolean;
+  nbaFranchise: boolean;
+  leagues: {
+      standard: {
+          conference: string;
+          division: string;
+      };
+      utah: {
+        conference: string;
+        division: string;
+      };
+      sacramento: {
+        conference: string;
+        division: string;
+      };
+  };
+}
